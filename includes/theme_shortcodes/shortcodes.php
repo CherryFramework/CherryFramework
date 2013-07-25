28f754c7da63262aa4d072c4fc5422b93a7b44fe
cheh <chehovskiy.dima@gmail.com>
2013-07-25 11:58:10 +0300

    fix shortcode_video_preview

--- includes/theme_shortcodes/shortcodes.php
+++ includes/theme_shortcodes/shortcodes.php
@@ -773,65 +773,73 @@ if (!function_exists('shortcode_video')) {
 	add_shortcode('video', 'shortcode_video');
 }
 	if (!function_exists('shortcode_video_preview')) {
-		function shortcode_video_preview($atts, $content = null) {
-			extract(shortcode_atts(
-				array(
-					'post_url' => '',
-					'title' => '',
-					'date' => '',
-					'author' => '',
-					'custom_class' => '',
-			), $atts));
-			$output_title = '';
-			$output_author = '';
-			$output_date = '';
-			$post_ID = url_to_postid($post_url);
-			$post = get_post($post_ID);
-			$get_user = get_userdata($post->post_author);
-			$user_url = get_bloginfo('url').'/author/'.$get_user->user_nicename;
-			$video_url = parser_video_url(get_post_meta($post_ID, 'tz_video_embed', true));
-			$get_image_url = video_image($video_url);
+	  function shortcode_video_preview($atts, $content = null) {
+	   extract(shortcode_atts(
+		array(
+		 'title' => '',
+		 'post_url' => '',
+		 'date' => '',
+		 'author' => '',
+		 'custom_class' => '',
+	   ), $atts));
+	   $output_title = '';
+	   $output_author = '';
+	   $output_date = '';
+	   $post_ID = url_to_postid($post_url);
+	   $get_post = get_post($post_ID);
+	   $get_user = get_userdata($get_post->post_author);
+	   $user_url = get_bloginfo('url').'/author/'.$get_user->user_nicename;
+	   $video_url = parser_video_url(get_post_meta($post_ID, 'tz_video_embed', true));
+	   $get_image_url = video_image($video_url);
+	   $img='';
 
-			if($title=="yes"){
-				$output_title = '<h4><a href="'.$post_url.'" title="'.$post->post_title.'">'.$post->post_title.'</a></h4>';
-			}
-			if($title=="yes"){
-				$output_author = '<span class="post_author">Posts by <a href="'.$user_url.'" title="Posts by '.$get_user->user_nicename.'"  rel="author">'.$get_user->user_nicename.'</a></span>';
-			}
-			if($title=="yes"){
-				$output_date = '<span class="post_date"><time datetime="'.$post->post_date.'"> '.get_the_time('M j, Y', $post_ID).'</time></span>';
-			}
-			$output ='<figure class="featured-thumbnail thumbnail video_preview '.$custom_class.'"><div><a class="preview_image"  href="'.$post_url.'" title="'.$get_image_url.'"><img src="'.$get_image_url.'" alt=""><span class="icon-play-circle hover"></span></a><figcaption>'.$output_title.$output_author.$output_date.'</figcaption></div></figure>';
+	   if($title=="yes"){
+		$output_title = '<h4><a href="'.$post_url.'" title="'.$get_post->post_title.'">'.$get_post->post_title.'</a></h4>';
+	   }
+	   if($author=="yes"){
+		$output_author = '<span class="post_author">Posts by <a href="'.$user_url.'" title="Posts by '.$get_user->user_nicename.'"  rel="author">'.$get_user->user_nicename.'</a></span>';
+	   }
+	   if($date=="yes"){
+		$output_date = '<span class="post_date"><time datetime="'.$get_post->post_date.'"> '.get_the_time('M j, Y', $post_ID).'</time></span>';
+	   }
+	   if($get_image_url!=false && $get_image_url!=''){
+		$img = '<a class="preview_image"  href="'.$post_url.'" title="'.$get_image_url.'"><img src="'.$get_image_url.'" alt=""><span class="icon-play-circle hover"></span></a>';
+	   }
+	   $output ='<figure class="featured-thumbnail thumbnail video_preview clearfix'.$custom_class.'"><div>'.$img.'<figcaption>'.$output_title.$output_author.$output_date.'</figcaption></div></figure>';
 
-			return $output;
+	   return $output;
+	  }
+	  add_shortcode('video_preview', 'shortcode_video_preview');
+	 }
+	 if (!function_exists('parser_video_url')) {
+	  function parser_video_url($video_url){
+	   $video_url = explode(" ", $video_url);
+	   foreach ($video_url as $item) {
+		if(stripos($item, 'src')!==false){
+		 $url_array = parse_url($item);
+		 $video_url = $url_array["path"];
+		 $video_url = stripcslashes($video_url);
+		 $video_url = strip_tags($video_url);
+		 $video_url = str_replace('&quot;', '', $video_url);
+		 break;
 		}
-		add_shortcode('video_preview', 'shortcode_video_preview');
-	}
-	if (!function_exists('parser_video_url')) {
-		function parser_video_url($video_url){
-			$video_url = explode(" ", $video_url);
-			foreach ($video_url as $item) {
-				if(stripos($item, 'src')!==false){
-					$url_array = parse_url($item);
-					$video_url = $url_array["path"];
-					$video_url = stripcslashes($video_url);
-					$video_url = strip_tags($video_url);
-					$video_url = str_replace('&quot;', '', $video_url);
-					break;
-				}
-			}
-			return $video_url;
+	   }
+	   return $video_url;
+	  }
+	 }
+	 if (!function_exists('video_image')) {
+	  function video_image($url){
+	   if($url[0]!==''){
+		$image_id = basename($url);
+		if(stripos($url, "youtube")!==false){
+		 return "http://img.youtube.com/vi/".$image_id."/0.jpg";
+		} else if(stripos($url, "vimeo")!==false){
+		 $hash = unserialize(file_get_contents("http://vimeo.com/api/v2/video/".$image_id.".php"));
+		 return $hash[0]["thumbnail_large"];
 		}
-	}
-	if (!function_exists('video_image')) {
-		function video_image($url){
-			$image_id = basename($url);
-			if(stripos($url, "youtube")!==false){
-				return "http://img.youtube.com/vi/".$image_id."/0.jpg";
-			} else if(stripos($url, "vimeo")!==false){
-				$hash = unserialize(file_get_contents("http://vimeo.com/api/v2/video/".$image_id.".php"));
-				return $hash[0]["thumbnail_large"];
-			}
-		}
-	}
+	   }else{
+		return false;
+	   }
+	  }
+	 }
 ?>
