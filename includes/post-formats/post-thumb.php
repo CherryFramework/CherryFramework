0872a0dcb1ad807b9d12aef3037345ba2ea6f03c
cheh <chehovskiy.dima@gmail.com>
2013-08-07 18:07:32 +0300

    fix prev commit

--- includes/post-formats/post-thumb.php
+++ includes/post-formats/post-thumb.php
@@ -5,17 +5,17 @@
 	$img_width    = 770; //set width large img
 	$img_height   = 380; //set height large img
 	$figure_class = "large";
-	$img_attr = (of_get_option('load_image') == 'false' || of_get_option('load_image')=="")?'src="':'data-src="';
+	$img_attr = (of_get_option('load_image') == 'false' || of_get_option('load_image')=="")?'src="':'src="//" data-src="';
 
 	if($post_image_size=='' && has_post_thumbnail() || $post_image_size=='normal' && has_post_thumbnail()){
 		$imgdata      = explode(' ', get_the_post_thumbnail());
-		$img_width    = floatval(ereg_replace("[^-0-9\.]","",$imgdata[1]));
-		$img_height   = floatval(ereg_replace("[^-0-9\.]","",$imgdata[2]));
+		$img_width    = intval(substr($imgdata[1], stripos($imgdata[1], '"')+1, strrpos($imgdata[1], '"')-1));
+		$img_height   = intval(substr($imgdata[2], stripos($imgdata[2], '"')+1, strrpos($imgdata[2], '"')-1));
 		$figure_class = "";
 	}
 	
 	$image = $img_attr.aq_resize($img_url, $img_width, $img_height, true).'"'; //resize & crop img
 	if(has_post_thumbnail()) {
-		echo '<figure class="featured-thumbnail thumbnail '.$figure_class.'"><a href="'.get_permalink().'" title="'.get_the_title().'"><img '.$image.' alt="'.get_the_title().'" /></a></figure>';
+		echo '<figure class="featured-thumbnail thumbnail '.$figure_class.'" ><a href="'.get_permalink().'" title="'.get_the_title().'" ><img '.$image.' alt="'.get_the_title().'" ></a></figure>';
 	};
 ?>
