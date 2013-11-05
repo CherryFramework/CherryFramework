fd958b0c4f6e0a38459aa3968a91b2905b8409c3
Behaart <makhonkodenis@gmail.com>
2013-11-05 12:57:34 +0200

    fix prev commit

--- header.php
+++ header.php
@@ -85,7 +85,7 @@
 				},
 				scaleFix = function () {
 					if (viewportmeta && /iPhone|iPad/.test(ua) && !/Opera Mini/.test(ua)) {
-						viewportmeta.content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+						viewportmeta.content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6, initial-scale=1.0";
 						document.addEventListener("gesturestart", gestureStart, false);
 					}
 				};
