bec45b619092ea43455093d76896ab758a3221d6
Behaart <makhonkodenis@gmail.com>
2013-11-06 17:49:41 +0200

    Add filter for slider

--- slider.php
+++ slider.php
@@ -3,7 +3,6 @@
 
 	// WPML filter
 	$suppress_filters = get_option('suppress_filters');
-
 	// query
 	$args = array(
 		'post_type'        => 'slider',
@@ -15,6 +14,28 @@
 		);
 	$slides = get_posts($args);
 	if (empty($slides)) return;
+
+	$slider_default_params = array(
+		"alignment"      => "'topCenter'", //topLeft, topCenter, topRight, centerLeft, center, centerRight, bottomLeft, bottomCenter, bottomRight
+		"barDirection"   => "'leftToRight'", //'leftToRight', 'rightToLeft', 'topToBottom', 'bottomToTop'
+		"barPosition"    => "'top'", //'bottom', 'left', 'top', 'right'
+		"easing"         => "'easeOutQuad'",
+		"mobileEasing"   => "''",
+		"mobileFx"       => "''",
+		"gridDifference" => "250",
+		"imagePath"      => "'images/'",
+		"minHeight"      => "'147px'", //you can also leave it blank
+		"height"         => "'47.4%'", //here you can type pixels (for instance '300px'), a percentage (relative to the width of the slideshow, for instance '50%') or 'auto'
+		"loaderColor"    => "'#ffffff'",
+		"loaderBgColor"  => "'#eb8a7c'",
+		"loaderOpacity"  => "1", //0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1
+		"loaderPadding"  => "0", //how many empty pixels you want to display between the loader and its background
+		"loaderStroke"   => "3", //the thickness both of the pie loader and of the bar loader. Remember: for the pie, the loader thickness must be less than a half of the pie diameter
+		"pieDiameter"    => "33",
+		"piePosition"    => "'rightTop'",
+		"portrait"       => "true"
+	);
+	$slider_filtered_params = apply_filters( 'cherry_slider_params', $slider_default_params );
 ?>
 
 <script type="text/javascript">
@@ -24,34 +45,15 @@
 			if (!myCamera.hasClass('motopress-camera')) {
 				myCamera.addClass('motopress-camera');
 				myCamera.camera({
-					alignment           : 'topCenter', //topLeft, topCenter, topRight, centerLeft, center, centerRight, bottomLeft, bottomCenter, bottomRight
 					autoAdvance         : <?php echo of_get_option('sl_slideshow'); ?>, //true, false
 					mobileAutoAdvance   : <?php echo of_get_option('sl_slideshow'); ?>, //true, false. Auto-advancing for mobile devices
-					barDirection        : 'leftToRight', //'leftToRight', 'rightToLeft', 'topToBottom', 'bottomToTop'
-					barPosition         : 'top', //'bottom', 'left', 'top', 'right'
 					cols                : <?php echo of_get_option('sl_columns'); ?>,
-					easing              : 'easeOutQuad', //for the complete list http://jqueryui.com/demos/effect/easing.html
-					mobileEasing        : '', //leave empty if you want to display the same easing on mobile devices and on desktop etc.
 					fx                  : "<?php echo of_get_option('sl_effect'); ?>", //'random','simpleFade', 'curtainTopLeft', 'curtainTopRight', 'curtainBottomLeft',          'curtainBottomRight', 'curtainSliceLeft', 'curtainSliceRight', 'blindCurtainTopLeft', 'blindCurtainTopRight', 'blindCurtainBottomLeft', 'blindCurtainBottomRight', 'blindCurtainSliceBottom', 'blindCurtainSliceTop', 'stampede', 'mosaic', 'mosaicReverse', 'mosaicRandom', 'mosaicSpiral', 'mosaicSpiralReverse', 'topLeftBottomRight', 'bottomRightTopLeft', 'bottomLeftTopRight', 'bottomLeftTopRight'
-													//you can also use more than one effect, just separate them with commas: 'simpleFade, scrollRight, scrollBottom'
-					mobileFx            : '', //leave empty if you want to display the same effect on mobile devices and on desktop etc.
-					gridDifference      : 250, //to make the grid blocks slower than the slices, this value must be smaller than transPeriod
-					height              : '47.4%', //here you can type pixels (for instance '300px'), a percentage (relative to the width of the slideshow, for instance '50%') or 'auto'
-					imagePath           : 'images/', //he path to the image folder (it serves for the blank.gif, when you want to display videos)
 					loader              : "<?php echo of_get_option('sl_loader'); ?>", //pie, bar, none (even if you choose "pie", old browsers like IE8- can't display it... they will display always a loading bar)
-					loaderColor         : '#ffffff',
-					loaderBgColor       : '#eb8a7c',
-					loaderOpacity       : 1, //0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1
-					loaderPadding       : 0, //how many empty pixels you want to display between the loader and its background
-					loaderStroke        : 3, //the thickness both of the pie loader and of the bar loader. Remember: for the pie, the loader thickness must be less than a half of the pie diameter
-					minHeight           : '147px', //you can also leave it blank
 					navigation          : <?php echo of_get_option('sl_dir_nav'); ?>, //true or false, to display or not the navigation buttons
 					navigationHover     : <?php echo of_get_option('sl_dir_nav_hide'); ?>, //if true the navigation button (prev, next and play/stop buttons) will be visible on hover state only, if false they will be visible always
 					pagination          : <?php echo of_get_option('sl_control_nav'); ?>,
 					playPause           : <?php echo of_get_option('sl_play_pause_button'); ?>, //true or false, to display or not the play/pause buttons
-					pieDiameter         : 33,
-					piePosition         : 'rightTop', //'rightTop', 'leftTop', 'leftBottom', 'rightBottom'
-					portrait            : true, //true, false. Select true if you don't want that your images are cropped
 					rows                : <?php echo of_get_option('sl_rows'); ?>,
 					slicedCols          : <?php echo of_get_option('sl_columns'); ?>,
 					slicedRows          : <?php echo of_get_option('sl_rows'); ?>,
@@ -59,8 +61,13 @@
 					time                : <?php echo of_get_option('sl_pausetime'); ?>, //milliseconds between the end of the sliding effect and the start of the next one
 					transPeriod         : <?php echo of_get_option('sl_animation_speed'); ?>, //lenght of the sliding effect in milliseconds
 
-									////////callbacks
-
+					<?php
+					//filtered params output
+					foreach ($slider_filtered_params as $param => $param_value) {
+						echo $param . " : " . $param_value . ",\n";
+					}
+					?>
+					////////callbacks
 					onEndTransition     : function(){}, //this callback is invoked when the transition effect ends
 					onLoaded            : function(){}, //this callback is invoked when the image on a slide has completely loaded
 					onStartLoading      : function(){}, //this callback is invoked when the image on a slide start loading
