1d10bd4fb54ef1f61f12ca6b87c3a8056c537f82
MakhonkoDenis <makhonkodenis@gmail.com>
2014-01-27 12:15:02 +0200

    Moved the help tips from child theme in the framework

--- functions.php
+++ functions.php
@@ -363,6 +363,44 @@
 	//remove auto loading rel=next post link in header
 	remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
 
+
+	// WP Pointers
+	if (!function_exists('myHelpPointers')) {
+		add_action('admin_enqueue_scripts', 'myHelpPointers');
+
+		function myHelpPointers() {
+			//First we define our pointers 
+			$pointers = array(
+				array(
+					'id'       => 'xyz1', // unique id for this pointer
+					'screen'   => 'themes', // this is the page hook we want our pointer to show on
+					'target'   => '#toplevel_page_options-framework', // the css selector for the pointer to be tied to, best to use ID's
+					'title'    => theme_locals("import_sample_data"),
+					'content'  => theme_locals("import_sample_data_desc"),
+					'position' => array( 
+										'edge'   => 'left', //top, bottom, left, right
+										'align'  => 'left', //top, bottom, left, right, middle
+										)
+					),
+				array(
+					'id'       => 'xyz2', // unique id for this pointer
+					'screen'   => 'toplevel_page_options-framework', // this is the page hook we want our pointer to show on
+					'target'   => '#toplevel_page_cherry-plugin-page', // the css selector for the pointer to be tied to, best to use ID's
+					'title'    => theme_locals("import_sample_data"),
+					'content'  => theme_locals("import_sample_data_desc"),
+					'position' => array(
+										'edge'   => 'left', //top, bottom, left, right
+										'align'  => 'left', //top, bottom, left, right, middle
+										)
+					)
+				// more as needed
+			);
+
+			//Now we instantiate the class and pass our pointer array to the constructor 
+			$myPointers = new WP_Help_Pointer($pointers);
+		};
+	}
+
 	/*
 	 * Navigation with description
 	 *
