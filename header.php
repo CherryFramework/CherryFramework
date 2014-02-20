3d4c16cb7733a2576cb85fdf79e66bd6692e8231
MakhonkoDenis <makhonkodenis@gmail.com>
2014-02-20 12:40:28 +0200

    fixed mobile menu in ie9

--- header.php
+++ header.php
@@ -53,13 +53,13 @@
 		<a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" alt="" /></a>
 	</div>
 	<![endif]-->
-	<!--[if (gt IE 9)|!(IE)]><!-->
-	<script src="<?php echo PARENT_URL; ?>/js/jquery.mobile.customized.min.js" type="text/javascript"></script>
-	<script type="text/javascript">
-		jQuery(function(){
-			jQuery('.sf-menu').mobileMenu({defaultText: <?php echo '"'.of_get_option('mobile_menu_label').'"'; ?>});
-		});
-	</script>
+	<!--[if gte IE 9]><!-->
+		<script src="<?php echo PARENT_URL; ?>/js/jquery.mobile.customized.min.js" type="text/javascript"></script>
+		<script type="text/javascript">
+			jQuery(function(){
+				jQuery('.sf-menu').mobileMenu({defaultText: <?php echo '"'.of_get_option('mobile_menu_label').'"'; ?>});
+			});
+		</script>
 	<!--<![endif]-->
 	<script type="text/javascript">
 		// Init navigation menu
