6ef0c4d98142ddd4f4a8799494b2c53fb27a4831
cheh <chehovskiy.dima@gmail.com>
2014-04-24 14:03:15 +0300

    Restored required cherry-plugin version

--- includes/register-plugins.php
+++ includes/register-plugins.php
@@ -56,7 +56,7 @@ function my_theme_register_required_plugins() {
 			'slug'               => 'cherry-plugin', // The plugin slug (typically the folder name)
 			'source'             => PARENT_DIR . '/includes/plugins/cherry-plugin.zip', // The plugin source
 			'required'           => true, // If false, the plugin is only 'recommended' instead of required
-			'version'            => '1.2', // E.g. 1.0.0. If set, the active plugin must be this version or higher, otherwise a notice is presented
+			'version'            => '1.1', // E.g. 1.0.0. If set, the active plugin must be this version or higher, otherwise a notice is presented
 			'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch
 			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins
 			'external_url'       => '', // If set, overrides default API URL and points to an external URL
