2d43ca4ca9acff998cdbe807747ba1f4b1b5f569
shinTM <emasugeta@gmail.com>
2014-06-26 15:37:10 +0300

    upd: stickUp.js

--- header.php
+++ header.php
@@ -94,12 +94,13 @@
 	</script>
 	<!-- stick up menu -->
 	<script type="text/javascript">
-
 		jQuery(document).ready(function(){
 			if(!device.mobile() && !device.tablet()){
 				jQuery('<?php echo apply_filters( "cherry_stickmenu_selector", ".header .nav__primary" ); ?>').tmStickUp({
 					correctionSelector: jQuery('#wpadminbar')
+				,	listenSelector: jQuery('<?php echo apply_filters( "cherry_stickmenu_listen_selector", ".listenSelector" ); ?>')
 				,	active: <?php echo (of_get_option('stickup_menu', 'false')=="false") ? 'false' : 'true'; ?>
+				,   pseudo: <?php echo apply_filters( "cherry_stickmenu_option_pseudo", "true" ); ?>
 				});
 			}
 		})
