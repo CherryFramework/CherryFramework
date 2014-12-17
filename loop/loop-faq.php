576b46f146d1f3820a74c65003e7afe7ac6ee942
cheh <chehovskiy.dima@gmail.com>
2014-12-17 12:24:24 +0200

    Fixed a query variable

--- loop/loop-faq.php
+++ loop/loop-faq.php
@@ -9,18 +9,16 @@
 		</div><!--post-->
 	<?php endwhile;
 	//query
-	$temp     = $wp_query;
-	$wp_query = null;
 	$args = array(
 		'post_type'        => 'faq',
 		'showposts'        => -1,
-		'suppress_filters' => $suppress_filters
+		'suppress_filters' => $suppress_filters,
 		);
-	$wp_query = new WP_Query($args);
+	$faq_query = new WP_Query( $args );
 
-	if (have_posts()) : ?>
+	if ( $faq_query->have_posts() ) : ?>
 	<dl class="faq-list">
-	<?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+	<?php while ( $faq_query->have_posts() ) : $faq_query->the_post(); ?>
 		<dt class="faq-list_h">
 			<h4 class="marker"><?php echo theme_locals("q"); ?></h4>
 			<h4><?php the_title(); ?></h4>
@@ -31,13 +29,15 @@
 		</dd>
 	<?php endwhile; ?>
 	</dl>
+
 <?php else: ?>
+
 <div class="no-results">
 	<?php echo '<p><strong>' . theme_locals("there_has") . '</strong></p>'; ?>
 	<p><?php echo theme_locals("we_apologize"); ?> <a href="<?php echo home_url(); ?>/" title="<?php bloginfo('description'); ?>"><?php echo theme_locals("return_to"); ?></a> <?php echo theme_locals("search_form"); ?></p>
 	<?php get_search_form(); /* outputs the default Wordpress search form */ ?>
 </div><!--.no-results-->
+
 <?php endif;
-	$wp_query = null;
-	$wp_query = $temp;
+	wp_reset_postdata();
 ?>
--- loop/loop-testi.php
+++ loop/loop-testi.php
@@ -21,11 +21,11 @@ $args = array(
 	'post_type'        => 'testi',
 	'showposts'        => 4,
 	'paged'            => $paged,
-	'suppress_filters' => $suppress_filters
+	'suppress_filters' => $suppress_filters,
 	);
-$wp_query = new WP_Query($args);
+$testi_query = new WP_Query( $args );
 
-if ($wp_query->have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_post();
+if ( $testi_query->have_posts()) : while ( $testi_query->have_posts() ) : $testi_query->the_post();
 	$testiname  = get_post_meta( $post->ID, 'my_testi_caption', true );
 	$testiurl   = esc_url( get_post_meta( $post->ID, 'my_testi_url', true ) );
 	$testiinfo  = get_post_meta( $post->ID, 'my_testi_info', true );
@@ -33,9 +33,9 @@ if ($wp_query->have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_
 ?>
 <article id="post-<?php the_ID(); ?>" class="testimonial clearfix">
 	<blockquote class="testimonial_bq">
-		<?php if(has_post_thumbnail()) {
+		<?php if ( has_post_thumbnail() ) {
 			$thumb   = get_post_thumbnail_id();
-			$img_url = wp_get_attachment_url( $thumb,'full'); //get img URL
+			$img_url = wp_get_attachment_url( $thumb,'full' ); //get img URL
 			$image   = aq_resize( $img_url, 120, 120, true ); //resize & crop img
 		?>
 		<figure class="featured-thumbnail thumbnail hidden-phone">
@@ -62,15 +62,17 @@ if ($wp_query->have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_
 		</div>
 	</blockquote>
 </article>
+
 <?php endwhile; else: ?>
+
 <div class="no-results">
 	<?php echo '<p><strong>' . theme_locals("there_has") . '</strong></p>'; ?>
 	<p><?php echo theme_locals("we_apologize"); ?> <a href="<?php echo home_url(); ?>/" title="<?php bloginfo('description'); ?>"><?php echo theme_locals("return_to"); ?></a> <?php echo theme_locals("search_form"); ?></p>
 	<?php get_search_form(); /* outputs the default Wordpress search form */ ?>
 </div><!--no-results-->
-<?php endif; ?>
 
-<?php
+<?php endif;
+
 	get_template_part('includes/post-formats/post-nav');
 	wp_reset_postdata();
 ?>
