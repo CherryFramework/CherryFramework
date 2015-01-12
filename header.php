15a7a585ccfba9a8ef58908b29ca0a9b8c92cdbf
cheh <chehovskiy.dima@gmail.com>
2015-01-12 17:31:55 +0200

    Updated a conditional comments for older IE - if lt IE 9

--- header.php
+++ header.php
@@ -47,9 +47,9 @@
 		wp_head();
 	?>
 	<?php /* The HTML5 Shim is required for older browsers, mainly older versions IE */ ?>
-	<!--[if lt IE 8]>
+	<!--[if lt IE 9]>
 		<div id="ie7-alert" style="width: 100%; text-align:center;">
-			<img src="http://tmbhtest.com/images/ie7.jpg" alt="Upgrade IE 7" width="640" height="344" border="0" usemap="#Map" />
+			<img src="http://tmbhtest.com/images/ie7.jpg" alt="Upgrade IE 8" width="640" height="344" border="0" usemap="#Map" />
 			<map name="Map" id="Map"><area shape="rect" coords="496,201,604,329" href="http://www.microsoft.com/windows/internet-explorer/default.aspx" target="_blank" alt="Download Interent Explorer" /><area shape="rect" coords="380,201,488,329" href="http://www.apple.com/safari/download/" target="_blank" alt="Download Apple Safari" /><area shape="rect" coords="268,202,376,330" href="http://www.opera.com/download/" target="_blank" alt="Download Opera" /><area shape="rect" coords="155,202,263,330" href="http://www.mozilla.com/" target="_blank" alt="Download Firefox" /><area shape="rect" coords="35,201,143,329" href="http://www.google.com/chrome" target="_blank" alt="Download Google Chrome" />
 			</map>
 		</div>
