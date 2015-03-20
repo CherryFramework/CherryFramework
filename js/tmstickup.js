e48eb3fb49495f2ca97380407e055405ed0b96bd
cheh <chehovskiy.dima@gmail.com>
2015-03-20 15:40:05 +0200

    FIX: stick menu script

--- js/tmstickup.js
+++ js/tmstickup.js
@@ -1,13 +1,13 @@
 (function($){
-	$.fn.tmStickUp=function(options){ 
-		
+	$.fn.tmStickUp=function(options){
+
 		var getOptions = {
 			correctionSelector: $('.correctionSelector')
 		,	listenSelector: $('.listenSelector')
 		,	active: false
 		,	pseudo: true
 		}
-		$.extend(getOptions, options); 
+		$.extend(getOptions, options);
 
 		var
 			_this = $(this)
@@ -78,6 +78,14 @@
 					}
 				}
 			}).trigger('scroll');
+
+			_document.on("resize", function() {
+				if(_this.hasClass('isStuck')){
+					if( thisOffsetTop != parseInt(pseudoBlock.offset().top) ) thisOffsetTop = parseInt(pseudoBlock.offset().top);
+				} else {
+					if( thisOffsetTop != parseInt(_this.offset().top) ) thisOffsetTop = parseInt(_this.offset().top);
+				}
+			})
 		}
 	}//end tmStickUp function
 })(jQuery)
