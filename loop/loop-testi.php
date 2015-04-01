0f05d59c54023e5bdce3250c575ddf764a481684
cheh <chehovskiy.dima@gmail.com>
2015-04-01 16:03:44 +0300

    FIX: pagination on testimonial page

--- loop/loop-testi.php
+++ loop/loop-testi.php
@@ -73,6 +73,20 @@ if ( $testi_query->have_posts()) : while ( $testi_query->have_posts() ) : $testi
 
 <?php endif;
 
-	get_template_part('includes/post-formats/post-nav');
+	if ( function_exists('pagination') ) :
+		pagination( $testi_query->max_num_pages );
+	else :
+		if ( $testi_query->max_num_pages > 1 ) : ?>
+		<ul class="pager">
+			<li class="previous">
+				<?php next_posts_link(theme_locals("older")) ?>
+			</li><!--.older-->
+			<li class="next">
+				<?php previous_posts_link(theme_locals("newer")) ?>
+			</li><!--.newer-->
+		</ul><!--.oldernewer-->
+		<?php endif;
+	endif;
+
 	wp_reset_postdata();
 ?>
