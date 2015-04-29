00b0cafc67fd13f872c145ab46c511545c38d125
cheh <chehovskiy.dima@gmail.com>
2015-04-29 11:51:57 +0300

    Remove inline CSS and JS from WP emoji support (only IE)

--- functions.php
+++ functions.php
@@ -1700,4 +1700,19 @@ function cherry_get_post_networks( $args = array() ) {
 
 		printf( '%s', $output );
 	}
+
+
+	/**
+	 * Remove inline CSS and JS from WP emoji support (IE only).
+	 */
+	global $is_IE;
+	if ( $is_IE ) {
+		remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+		remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+		remove_action( 'wp_print_styles', 'print_emoji_styles' );
+		remove_action( 'admin_print_styles', 'print_emoji_styles' );
+		remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+		remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+		remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	}
 ?>
