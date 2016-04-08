c405eee336f6c617b05a1dd60164e2e57a615421
cheh <cheh@cheh.vpn>
2016-04-08 14:44:24 +0300

    UPD: force activation for `cherry-plugin`

--- includes/register-plugins.php
+++ includes/register-plugins.php
@@ -47,7 +47,7 @@ function cherry_register_required_plugins() {
 			'source'             => PARENT_DIR . '/includes/plugins/cherry-plugin.zip', // The plugin source.
 			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
 			'version'            => '1.1', // E.g. 1.0.0. If set, the active plugin must be this version or higher, otherwise a notice is presented.
-			'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
 			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
 			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
 		),
